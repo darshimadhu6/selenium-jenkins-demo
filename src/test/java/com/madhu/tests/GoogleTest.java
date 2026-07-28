adc220befa83375a449574e4490b066e8559cb1b
MADHU DARSHI S <madhu.darshi.s@ibm.com>
2026-07-28 18:49:39 +0530

    Initial Jenkins Demo

---
+++ src/test/java/com/madhu/tests/GoogleTest.java
@@ -0,0 +1,26 @@
+package com.madhu.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleTest {
+
+    @Test
+    public void openGoogle() {
+
+        WebDriverManager.firefoxdriver().setup();
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Google opened successfully");
+
+        driver.quit();
+    }
+}
