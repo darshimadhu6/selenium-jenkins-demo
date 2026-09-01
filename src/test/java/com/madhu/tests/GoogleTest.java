75dd49a32db8c39ce08883b019eee7fa724622a4
MADHU DARSHI S <madhu.darshi.s@ibm.com>
2026-09-01 21:57:46 +0530

    jenkins parameters

--- src/test/java/com/madhu/tests/GoogleTest.java
+++ src/test/java/com/madhu/tests/GoogleTest.java
@@ -11,26 +11,38 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 public class GoogleTest {
 
     @Test
-    public void openGoogle() {
+public void openGoogle() {
+    	
+    	String browser = System.getProperty("browser","firefox");
+    	WebDriver driver;
 
-        WebDriverManager.firefoxdriver().setup();
+    	if (browser.equalsIgnoreCase("firefox")) {
+    		
+    		WebDriverManager.firefoxdriver().setup();
 
-        FirefoxOptions options = new FirefoxOptions();
+            FirefoxOptions options = new FirefoxOptions();
 
-        options.addArguments("--headless");
-
-        WebDriver driver = new FirefoxDriver(options);
+            options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
+    		
+    	}
+    	else {
+    		
+    		 throw new IllegalArgumentException(
+    	                "Unsupported browser: " + browser
+    	        );
+    	}
 
         driver.manage().window().maximize();
 
         driver.get("https://www.google.com");
         
         Assert.assertTrue(
-                driver.getTitle().contains("Google"),
-                "Google title validation failed"
+        		 driver.getTitle().contains("Google"),
+                 "Google title validation our test failed"
             );
 
-        System.out.println("Google opened successfully");
+        System.out.println("Google opened successfully using: " + browser);
 
         driver.quit();
     }
